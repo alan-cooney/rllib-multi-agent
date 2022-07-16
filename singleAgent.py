5aac7eac2762d42cac9584d3b5ce1a9fbe670ab1
Alan <41682961+alan-cooney@users.noreply.github.com>
2022-07-16 11:32:59 +0000

    Clean up single agent code

--- singleAgent.py
+++ singleAgent.py
@@ -1,3 +1,5 @@
+from os.path import dirname, join, realpath
+
 import ray
 from ray import tune
 from ray.rllib.agents.ppo import PPOConfig
@@ -7,48 +9,40 @@ ray.init(configure_logging=False,
          logging_level="info")
 
 # Model overrides
-# From https://github.com/ray-project/ray/blob/master/rllib/tuned_examples/ppo/cartpole-ppo.yaml
+# https://github.com/ray-project/ray/blob/master/rllib/tuned_examples/ppo/cartpole-ppo.yaml
 model = {
-    "fcnet_hiddens": [32],
     "fcnet_activation": "linear",
-    "vf_share_layers": True
+    "fcnet_hiddens": [32],
+    "vf_share_layers": True,
 }
 
 # Config overrides
-# From https://github.com/ray-project/ray/blob/master/rllib/tuned_examples/ppo/cartpole-ppo.yaml
+# https://github.com/ray-project/ray/blob/master/rllib/tuned_examples/ppo/cartpole-ppo.yaml
 config = PPOConfig().framework(
     framework="torch"
 ).training(
     gamma=0.99,
     lr=0.0003,
-    vf_loss_coeff=0.01,
-    num_sgd_iter=6,
     model=model,
-    train_batch_size=4000
+    num_sgd_iter=6,
+    train_batch_size=4000,
+    vf_loss_coeff=0.01,
 ).rollouts(
+    num_rollout_workers=5,
     observation_filter="MeanStdFilter",
-    num_rollout_workers=5
 ).environment(
     env="CartPole-v0"
 ).resources(
     # num_gpus=1
 )
 
-# Setup the trainer
-# trainer = PPOTrainer(env="CartPole-v0", config=config.to_dict())
-
-# analysis = tune.run(
-#     "PPO",
-#     stop={"episode_reward_mean": 190},
-#     config=config.to_dict(),
-#     checkpoint_at_end=True)
-
-analysis = tune.run(
+# Train (note we use Ray Tune to enable hyperparmeter tuning in other
+# experiments, but here we only have one parameter).
+tune.run(
     "PPO",
-    stop={"episode_reward_mean": 190},
+    checkpoint_at_end=True,
     config=config.to_dict(),
-    checkpoint_at_end=True
+    local_dir=join(dirname(realpath(__file__)), ".results"),
+    resume=False,
+    stop={"episode_reward_mean": 190},
 )
-
-# or simply get the last checkpoint (with highest "training_iteration")
-# last_checkpoint = analysis.get_last_checkpoint()
